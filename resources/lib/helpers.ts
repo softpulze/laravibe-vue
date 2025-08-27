28f4d6017af1fcb85ed25ce0804efc56dd3ad9df
ashokatheheroo <ashokbaruaakas@gmail.com>
2025-08-27 15:51:02 +0600

    ✨ feat: add isCallable type guard

--- resources/lib/helpers.ts
+++ resources/lib/helpers.ts
@@ -2,6 +2,7 @@ import AccountLayout from '@/layouts/AccountLayout.vue';
 import AuthLayout from '@/layouts/AuthLayout.vue';
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
 import GuestLayout from '@/layouts/GuestLayout.vue';
+import type { Callable, VoidCallable } from '@/types';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 
@@ -17,3 +18,5 @@ export const resolveView = (name: string) => {
     pageComponent.then((pc) => (pc.default.layout = pc.default.layout || resolveLayout(name)));
     return pageComponent;
 };
+
+export const isCallable = (value: unknown): value is Callable | VoidCallable => typeof value === 'function';
