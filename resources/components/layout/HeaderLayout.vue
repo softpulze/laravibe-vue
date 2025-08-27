f39922320ad6ef3afb1effd97ca7d534e57a7639
ashokatheheroo <ashokbaruaakas@gmail.com>
2025-08-27 06:24:25 +0600

    ✨ feat(layout): add flexible header/sidebar layouts and content wrapper

---
+++ resources/components/layout/HeaderLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { LayoutContent, LayoutShell } from './';
+</script>
+
+<template>
+    <LayoutShell class="flex-col" variant="header">
+        <slot name="header" />
+
+        <LayoutContent variant="header">
+            <slot />
+        </LayoutContent>
+    </LayoutShell>
+</template>
---
+++ resources/components/layout/SidebarLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { LayoutContent, LayoutShell } from './';
+
+interface Props {
+    initiallyIsSidebarOpen?: boolean;
+}
+
+withDefaults(defineProps<Props>(), { initiallyIsSidebarOpen: true });
+</script>
+
+<template>
+    <LayoutShell variant="sidebar" :initiallyIsSidebarOpen="initiallyIsSidebarOpen">
+        <slot name="sidebar" />
+
+        <LayoutContent variant="sidebar" class="overflow-x-hidden">
+            <slot name="header" />
+
+            <slot />
+        </LayoutContent>
+    </LayoutShell>
+</template>
