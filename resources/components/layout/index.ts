5e2e4fb09dbfd9ce60f366f7aea06819f7be713f
ashokatheheroo <ashokbaruaakas@gmail.com>
2025-08-31 19:24:46 +0600

    refact(formatted): formatted with prettier

--- resources/components/layout/index.ts
+++ resources/components/layout/index.ts
@@ -4,6 +4,6 @@ export { default as SidebarLayout } from './SidebarLayout.vue';
 export { default as LayoutContent } from './Content.vue';
 export { default as LayoutShell } from './Shell.vue';
 
+export { default as AuthCardLayout } from './auth/CardLayout.vue';
 export { default as AuthSimpleLayout } from './auth/SimpleLayout.vue';
 export { default as AuthSplitLayout } from './auth/SplitLayout.vue';
-export { default as AuthCardLayout } from './auth/CardLayout.vue';
