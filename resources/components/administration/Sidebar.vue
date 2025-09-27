69fe070ba67dd492a66434b7fe8c9c77886a048d
ashokatheheroo <ashokbaruaakas@gmail.com>
2025-09-27 17:04:48 +0600

    update organization name to softpulze

--- resources/components/administration/Sidebar.vue
+++ resources/components/administration/Sidebar.vue
@@ -30,7 +30,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/softpulselab/laravibe-vue',
+        href: 'https://github.com/softpulze/laravibe-vue',
         icon: FolderGit,
     },
 ];
