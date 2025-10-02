7c9f4f4cf91b62fecc8c6583ca95eafafe77d4ab
ashokatheheroo <ashokbaruaakas@gmail.com>
2025-10-03 00:40:24 +0600

    feat(heading): add classNames prop and cn utility to allow custom classes

--- resources/components/Heading.vue
+++ resources/components/Heading.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import { cn } from '@/lib/utils';
+import { HTMLAttributes } from 'vue';
+
 interface Props {
     title: string;
     description?: string;
+    classNames?: HTMLAttributes['class'];
 }
 
 defineProps<Props>();
 </script>
 
 <template>
-    <div class="mb-8 space-y-0.5">
+    <div :class="cn('mb-8 space-y-0.5', classNames)">
         <h2 class="text-xl font-semibold tracking-tight">{{ title }}</h2>
         <p v-if="description" class="text-sm text-muted-foreground">
             {{ description }}
