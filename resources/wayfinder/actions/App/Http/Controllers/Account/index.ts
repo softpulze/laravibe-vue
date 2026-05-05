4239c155ae9f6241732126a117a0175a3ab00d22
Ashok <ashokbaruaakas@gmail.com>
2026-05-05 17:57:37 +0600

    Untracked wayfinder files

--- resources/wayfinder/actions/App/Http/Controllers/Account/index.ts
+++
@@ -1,9 +0,0 @@
-import AccountController from './AccountController'
-import SecurityController from './SecurityController'
-
-const Account = {
-    AccountController: Object.assign(AccountController, AccountController),
-    SecurityController: Object.assign(SecurityController, SecurityController),
-}
-
-export default Account
--- resources/wayfinder/actions/App/Http/Controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import Account from './Account'
-import Auth from './Auth'
-
-const Controllers = {
-    Account: Object.assign(Account, Account),
-    Auth: Object.assign(Auth, Auth),
-}
-
-export default Controllers
